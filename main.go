107212c7e8a2ae38b6c5d1d696157f4956f4eb68
Phillip Whelan <phil@calyptia.com>
2022-05-06 03:06:10 -0400

    use json for configuration.

--- main.go
+++ main.go
@@ -1,40 +1,77 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"strconv"
+	"time"
+
 	"github.com/pwhelan/gousb"
 )
 
+type ID gousb.ID
+
+type config struct {
+	Product ID       `json:"product"`
+	Vendor  ID       `json:"vendor"`
+	CmdUp   []string `json:"up"`
+	CmdDown []string `json:"down"`
+}
+
+func (id *ID) UnmarshalJSON(data []byte) error {
+	var num string
+	if err := json.Unmarshal(data, &num); err != nil {
+		return err
+	}
+	val, err := strconv.ParseUint(num[2:], 16, 32)
+	if err != nil {
+		return err
+	}
+	*id = ID(val)
+	return nil
+}
+
+type configs []config
+
 func main() {
+	cfgs := make(configs, 0)
 	usb := gousb.NewContext()
+
+	cfgdata, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(cfgdata, &cfgs); err != nil {
+		panic(err)
+	}
+
 	usb.RegisterHotplug(func(ev gousb.HotplugEvent) {
 		desc, err := ev.DeviceDesc()
 		if err != nil {
 			panic(err)
 		}
-		// 445a:1015
-		// ./m1ddc display 1 set input 17 ; sleep 5 ; ./m1ddc display 1 set input 6
-		if desc.Vendor == 0x05e3 && desc.Product == 0x0610 {
-			fmt.Println("PWNEDDD!")
-			if ev.Type() == gousb.HotplugEventDeviceArrived {
-				fmt.Println("ADDED")
-				cmd := exec.Command("/Users/pwhelan/bin/m1ddc",
-					"display", "1", "set", "input", "6")
-				if err := cmd.Run(); err != nil {
-					fmt.Printf("ERROR: %w", err)
-				}
-			} else if ev.Type() == gousb.HotplugEventDeviceLeft {
-				fmt.Println("HAS LEFT")
-				cmd := exec.Command("/Users/pwhelan/bin/m1ddc",
-					"display", "1", "set", "input", "15")
-				if err := cmd.Run(); err != nil {
-					fmt.Printf("ERROR: %w", err)
+
+		for _, cfg := range cfgs {
+			if desc.Vendor == gousb.ID(cfg.Vendor) && desc.Product == gousb.ID(cfg.Product) {
+				if ev.Type() == gousb.HotplugEventDeviceArrived {
+					cmd := exec.Command(cfg.CmdUp[0], cfg.CmdUp[1:]...)
+					if err := cmd.Run(); err != nil {
+						fmt.Printf("ERROR: %s", err)
+					}
+				} else if ev.Type() == gousb.HotplugEventDeviceLeft {
+					cmd := exec.Command(cfg.CmdUp[0], cfg.CmdDown[1:]...)
+					if err := cmd.Run(); err != nil {
+						fmt.Printf("ERROR: %s", err)
+					}
 				}
 			}
 		}
 	})
+
 	for {
-		
+		time.Sleep(time.Second * 30)
 	}
 }
