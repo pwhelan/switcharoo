228f6cafb8d0324a10c54a284544800ee41eb101
Phillip Whelan <phil@calyptia.com>
2023-10-19 22:10:21 -0300

    Add support for watching for new programs.

--- main.go
+++ main.go
@@ -4,19 +4,27 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"time"
 
+	"github.com/elastic/gosigar/psnotify"
 	"github.com/pwhelan/gousb"
 )
 
 type ID gousb.ID
 
-type config struct {
+type usbhotplugconfig struct {
 	Product ID       `json:"product"`
 	Vendor  ID       `json:"vendor"`
+	CmdUp   []string `json:"up"`
+	CmdDown []string `json:"down"`
+}
+
+type execconfig struct {
+	Binary  string   `json:"bin"`
 	CmdUp   []string `json:"up"`
 	CmdDown []string `json:"down"`
 }
@@ -34,17 +42,28 @@ func (id *ID) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
-type configs []config
+type config struct {
+	USB  []usbhotplugconfig `json:"usb"`
+	Exec []execconfig       `json:"exec"`
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseInt(s, 10, 64)
+	return err == nil
+}
 
 func main() {
-	cfgs := make(configs, 0)
+	var cfg config
+	execs := make(map[string]execconfig)
+	execd := make(map[int]execconfig)
+
 	usb := gousb.NewContext()
 
 	cfgdata, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		panic(err)
 	}
-	if err := json.Unmarshal(cfgdata, &cfgs); err != nil {
+	if err := json.Unmarshal(cfgdata, &cfg); err != nil {
 		panic(err)
 	}
 
@@ -54,7 +73,7 @@ func main() {
 			panic(err)
 		}
 
-		for _, cfg := range cfgs {
+		for _, cfg := range cfg.USB {
 			if desc.Vendor == gousb.ID(cfg.Vendor) && desc.Product == gousb.ID(cfg.Product) {
 				if ev.Type() == gousb.HotplugEventDeviceArrived {
 					cmd := exec.Command(cfg.CmdUp[0], cfg.CmdUp[1:]...)
@@ -62,7 +81,7 @@ func main() {
 						fmt.Printf("ERROR: %s", err)
 					}
 				} else if ev.Type() == gousb.HotplugEventDeviceLeft {
-					cmd := exec.Command(cfg.CmdUp[0], cfg.CmdDown[1:]...)
+					cmd := exec.Command(cfg.CmdDown[0], cfg.CmdDown[1:]...)
 					if err := cmd.Run(); err != nil {
 						fmt.Printf("ERROR: %s", err)
 					}
@@ -70,6 +89,58 @@ func main() {
 			}
 		}
 	})
+
+	pswatcher, err := psnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, exec := range cfg.Exec {
+		execs[exec.Binary] = exec
+	}
+
+	go func() {
+		for {
+			select {
+			case ev := <-pswatcher.Exec:
+				bin, _ := os.Readlink(fmt.Sprintf("/proc/%d/exe", ev.Pid))
+				if ex, ok := execs[bin]; ok {
+					log.Printf("exec event: %d->%s", ev.Pid, bin)
+					cmd := exec.Command(ex.CmdUp[0], ex.CmdUp[1:]...)
+					if err := cmd.Run(); err != nil {
+						fmt.Printf("ERROR: %s\n", err)
+					}
+					execd[ev.Pid] = ex
+				}
+			case ev := <-pswatcher.Exit:
+				if ex, ok := execd[ev.Pid]; ok {
+					log.Printf("exit event: %d->%s (%+v)", ev.Pid, ex.Binary, ev)
+					cmd := exec.Command(ex.CmdDown[0], ex.CmdDown[1:]...)
+					if err := cmd.Run(); err != nil {
+						fmt.Printf("ERROR: %s\n", err)
+					}
+					delete(execd, ev.Pid)
+				}
+			}
+		}
+	}()
+
+	files, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.IsDir() && isNumeric(f.Name()) {
+			pid, _ := strconv.ParseInt(f.Name(), 10, 64)
+			err = pswatcher.Watch(int(pid), psnotify.PROC_EVENT_EXIT|psnotify.PROC_EVENT_EXEC)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+
+	defer pswatcher.Close()
 
 	for {
 		time.Sleep(time.Second * 30)
